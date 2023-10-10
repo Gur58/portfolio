a3c8f0f9c330a28fbe9ff64abda8bdb762f0df28
Evgenii_Gurev <EGurev@bellintegrator.com>
2023-10-10 17:54:16 +0300

    add display current language

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,7 +11,7 @@ import {useTranslation} from "react-i18next";
 
 function Header() {
     const { t } = useTranslation();
-    const { theme: { onChangeTheme }, language: { onChangeLanguage } } = useContext(Store);
+    const { theme: { onChangeTheme }, language: { value, onChangeLanguage } } = useContext(Store);
 
     const onClick = (id: string) => () => {
         document.getElementById(id).scrollIntoView({ behavior: "smooth" })
@@ -21,18 +21,18 @@ function Header() {
         <div className="flex justify-between items-center py-[1rem] h-16 rounded-xl">
             <div className="flex justify-start items-center">
                 <LogoIcon className="w-14 h-14 mr-5 hidden xsm:block md:mr-16 dark:text-[#b5b5b5] text-[#554949] hover:cursor-pointer active:bg-gray-200 rounded-full p-1.5" />
-                <div onClick={onClick('technology')} className="flex mr-2 md:mr-5 font-bold hover:underline underline-offset-4 select-none items-center p-2 hover:cursor-pointer active:bg-gray-200 rounded">
+                <button type="button" onClick={onClick('technology')} className="flex mr-2 md:mr-5 font-bold hover:underline underline-offset-4 select-none items-center p-2 hover:cursor-pointer active:bg-gray-200 rounded">
                     <TechIcon className="w-6 h-6 md:mr-2"/>
                     <span className="hidden lg:inline">{t('technology')}</span>
-                </div>
-                <div onClick={onClick('company')} className="flex mr-2 md:mr-5 font-bold hover:underline underline-offset-4 select-none items-center p-2 hover:cursor-pointer active:bg-gray-200 rounded">
+                </button>
+                <button type="button" onClick={onClick('company')} className="flex mr-2 md:mr-5 font-bold hover:underline underline-offset-4 select-none items-center p-2 hover:cursor-pointer active:bg-gray-200 rounded">
                     <CompanyIcon className="w-6 h-6 md:mr-2"/>
                     <span className="hidden lg:inline">{t('companies')}</span>
-                </div>
-                <div onClick={onClick('projects')} className="flex mr-2 md:mr-5 font-bold hover:underline underline-offset-4 select-none items-center p-2 hover:cursor-pointer active:bg-gray-200 rounded">
+                </button>
+                <button type="button" onClick={onClick('projects')} className="flex mr-2 md:mr-5 font-bold hover:underline underline-offset-4 select-none items-center p-2 hover:cursor-pointer active:bg-gray-200 rounded">
                     <ProjectsIcon className="w-6 h-6 md:mr-2"/>
                     <span className="hidden lg:inline">{t('projects')}</span>
-                </div>
+                </button>
             </div>
             <div className="flex">
                 <button type="button">
@@ -43,7 +43,8 @@ function Header() {
                         }
                     />
                 </button>
-                <button type="button">
+                <button type="button" className="relative">
+                    <span className="absolute top-[-7px] right-[-7px] text-[11px] font-bold">{value.toUpperCase()}</span>
                     <LanguageIcon
                         className="w-9 h-9 hover:cursor-pointer active:bg-gray-200 rounded-full p-1"
                         onClick={onChangeLanguage}
